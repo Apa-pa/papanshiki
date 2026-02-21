ba0778234848954baaa638eb70cfd626db603f42
Atsushi E <lasshi13@gmail.com>
2026-02-21 23:43:24 +0900

    feat: 分数マスター学習モジュールを追加し、関連ファイルを更新しました。

--- service-worker.js
+++ service-worker.js
@@ -56,6 +56,9 @@ var urlsToCache = [
   './kanji/grade1.js',
   './kanji/grade2.js',
 
+  // === 分数学習 ===
+  './bunsu_learn.html',
+
   // === 英語系 ===
   './alphabet.html',
   './alphabet_read.html',
